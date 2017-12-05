e1dff000c21ddf8cfa58c297d21f185f04cefffa
cesarcruz <cesar@logicsp.com.br>
2017-12-05 10:41:08 -0200

    Exemplo Comunicação GWT > Rest

---
+++ spring-boot-server/src/main/java/com/codecrafters/server/ScheduledDatabaseResetTask.java
@@ -0,0 +1,46 @@
+package com.codecrafters.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * This class is used as scheduled task which resets the database every 15 minutes to it's initial state.
+ *
+ * @author Logic
+ */
+@Component
+class ScheduledDatabaseResetTask {
+
+    private final Logger logger = LoggerFactory.getLogger(ScheduledDatabaseResetTask.class);
+    private final TodoItemRepository repository;
+    private final SpringBootGwtProperties springBootGwtProperties;
+
+    @Autowired
+    public ScheduledDatabaseResetTask(final TodoItemRepository repository, final SpringBootGwtProperties springBootGwtProperties) {
+        this.repository = repository;
+        this.springBootGwtProperties = springBootGwtProperties;
+    }
+
+    @Scheduled(fixedRateString = "${spring-boot-server.scheduled-database-reset-interval-millis}")
+    public void resetDatabase() {
+        if (springBootGwtProperties.isScheduledDatabaseReset()) {
+            logger.info("Reset database");
+
+            repository.deleteAll();
+
+            for (final String initialTodoItem : springBootGwtProperties.getInitialTodoItems()) {
+                repository.save(new TodoItem(initialTodoItem));
+            }
+
+            final List<TodoItem> itemsInDatabase = Optional.ofNullable(repository.findAll()).orElse(Collections.emptyList());
+            logger.info("Saved " + itemsInDatabase.size() + " todo items to the database: " + itemsInDatabase.toString());
+        }
+    }
+}
---
+++ spring-boot-server/src/test/groovy/com/codecrafters/server/TodoItemRestControllerIntegrationTest.groovy
@@ -0,0 +1,86 @@
+package com.codecrafters.server
+
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.embedded.LocalServerPort
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.ActiveProfiles
+import spock.lang.Shared
+import spock.lang.Specification
+import spock.lang.Stepwise
+
+import javax.ws.rs.core.HttpHeaders
+/**
+ * This class is used as integration test for the /todos url (TodoItemRestController).
+ *
+ * @author Logic
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Stepwise
+@ActiveProfiles("test")
+class TodoItemRestControllerIntegrationTest extends Specification {
+
+    @Shared
+    TestRestTemplate restTemplate = new TestRestTemplate()
+
+    @Autowired
+    ObjectMapper mapper
+
+    @LocalServerPort
+    private int port
+
+    def "PUT /todos"() {
+        when: "we post a new item"
+        def request = new HttpEntity<>(new TodoItem("Sample text 12354321423"))
+        def response = restTemplate.exchange(getTodosUrl(), HttpMethod.PUT, request, String.class)
+
+        then: "we get the corresponding http status"
+        response.getStatusCode() == HttpStatus.OK
+    }
+
+    def "GET /todos"() {
+        when: "we get the items"
+        def response = restTemplate.getForEntity(getTodosUrl(), String.class)
+
+        then: "we get the http status OK"
+        response.getStatusCode() == HttpStatus.OK
+
+        and: "the response should not be cached"
+        response.getHeaders().get(HttpHeaders.CACHE_CONTROL).get(0) == "no-cache"
+    }
+
+    def "GET /todos?text=123543"() {
+        when: "we search for an item by the text"
+        def responseExact = restTemplate.getForEntity(getTodosUrl() + "?text=123543", String.class)
+        List<TodoItem> items = mapper.readValue(responseExact.getBody(), new TypeReference<List<TodoItem>>() {})
+
+        then: "we get the item"
+        items.size() == 1
+        items.get(0).getText() == "Sample text 12354321423"
+    }
+
+    def "DELETE /todos"() {
+        given: "the item we saved in the requests before"
+        def responseBeforeDelete = restTemplate.getForEntity(getTodosUrl() + "?text=123543", String.class)
+        List<TodoItem> itemsBeforeDelete = mapper.readValue(responseBeforeDelete.getBody(), new TypeReference<List<TodoItem>>() {})
+
+        when: "we delete the item and request them afterwards again"
+        HttpEntity<TodoItem> request = new HttpEntity<>(itemsBeforeDelete.get(0))
+        restTemplate.exchange(getTodosUrl(), HttpMethod.DELETE, request, String.class)
+
+        def responseAfterDelete = restTemplate.getForEntity(getTodosUrl() + "?text=123543", String.class)
+        List<TodoItem> itemsAfterDelete = mapper.readValue(responseAfterDelete.getBody(), new TypeReference<List<TodoItem>>() {})
+
+        then: "the item does not exist"
+        itemsAfterDelete.size() == 0
+    }
+
+    String getTodosUrl() {
+        return "http://localhost:" + port + "/todos"
+    }
+}
